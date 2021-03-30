93bbe24d15831c0fd4b208b332049bd39e65b0b2
Lydia Burgert <burlyd00@service06.cluster.bc2.ch>
2021-03-30 09:57:10 +0200

    slight modification

--- 1_OM_basic_workflow/create_folders.R
+++ 1_OM_basic_workflow/create_folders.R
@@ -38,4 +38,7 @@ create_folders <- function(exp){
   file.copy(paste0("/scicore/home/penny/",user,"/M3TPP/analysisworkflow/analysis_scripts/PS_02_CompareSimulationResults_Original.R"), 
             paste0("/scicore/home/penny/",user,"/M3TPP/Experiments/",exp,"/analysis_scripts/PS_02_CompareSimulationResults_",exp,".R"),overwrite=TRUE)
   
+  file.copy(paste0("/scicore/home/penny/",user,"/M3TPP/analysisworkflow/analysis_scripts/PS_03_ContParamOutput_Original.R"), 
+            paste0("/scicore/home/penny/",user,"/M3TPP/Experiments/",exp,"/analysis_scripts/PS_03_ContParamOutput_",exp,".R"),overwrite=TRUE)
+  
 }
