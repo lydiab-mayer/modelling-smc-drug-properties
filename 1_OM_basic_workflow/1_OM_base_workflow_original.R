c9b98ba939e811592bef941d90f6bd773d90549a
Narimane Nekkab <nekkab0000@service06.cluster.bc2.ch>
2021-08-04 17:03:38 +0200

    clean up of step 1 script

--- 1_OM_basic_workflow/1_OM_base_workflow_original.R
+++ 1_OM_basic_workflow/1_OM_base_workflow_original.R
@@ -1,15 +1,42 @@
-##############################
-# Main script for specifying parameter values and running OpenMalaria simulations on the cluster. 
-# 
-#
-# created 12.02.2021
-# lydia.burgert@unibas.ch 
-#############################
+#####################################
+#####################################
+###                               ###
+### STEP 1: OM SETUP & SIMULATION ###
+###                               ###
+#####################################
+#####################################
 
-# Setup
+##########################################################################
+###
+### M3TPP PROJECT:
+### Main script for specifying parameter values 
+### and running OpenMalaria simulations on the cluster 
+### 
+### Original script:
+### Created 12.02.2021
+### lydia.burgert@unibas.ch 
+###
+### Adapted script:
+### 30.07.2021
+### narimane.nekkab@unibas.ch
+###
+### R version 3.6.0
+###
+##########################################################################
+
+
+##############
+### HEADER ###
+##############
+
+# Clear environment
 rm(list = ls())
-library(tgp)
+
+# Set seed for replication
 set.seed(42)
+
+# Library
+library(tgp)
 
 # User 
 user = strsplit(getwd(), "/", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][5]
@@ -21,6 +48,12 @@ setwd(paste0("/scicore/home/penny/",user,"/M3TPP"))
 source(paste0("./analysisworkflow/1_OM_basic_workflow/genOMsimscripts.R"))
 source(paste0("./analysisworkflow/1_OM_basic_workflow/generate_param_table.R"))
 source(paste0("./analysisworkflow/1_OM_basic_workflow/create_folders.R"))
+source(paste0("./Experiments/iTPP1_TestCase/convert_access.R"))
+
+
+##################
+### EXPERIMENT ###
+##################
 
 # Insert experiment name here
 exp ="..."
@@ -28,92 +61,151 @@ exp ="..."
 # Create folder in working directory
 create_folders(exp) # <----- run 1st time then comment
 
-# Seasonality type: monthly EIR vs. Fourier transformation (choose 1)
+# Optimize file size
+chunk_size = 90000
+
+
+##################
+### PARAMETERS ###
+##################
+
+# Specify the desired parameter values 
+# Note some values are categorical and others continuous
+# Values are sampled for continuous variables from defined range
+
+##################
+### DEMOGRAPHY ###
+
+# Population size
+pop_size = 10000
+
+# Demography dataframe
+demography=data.frame(pop_size=pop_size)
+
+##################
+### MONITORING ###
+
+# Years of analysis
+start_year = 2030
+end_year   = 2046
+
+# Number of years burn in
+burn_in_years = 80
+
+# Set the burn in relative to start year
+burn_in = start_year - burn_in_years
+
+# Reference survey age group (in years) (default for now)
+# 0 <= 0.25 = group 1
+# 0.25 <= 2 = group 2
+# 2 <= 5    = group 3
+# 5 <= 10   = group 4
+# 10 <= 15  = group 5
+# 15 <= 20  = group 6
+# 20 <= 100 = group 7
+
+# Monitoring dataframe
+monitoring=data.frame(start_year=start_year,
+                      end_year=end_year,
+                      burn_in=burn_in)
+
+####################
+### INTERVENTION ###
+
+# Efficacy range (in %)
+Efficacy= c(0.7,1)
+
+# Half-life range (in days)
+Halflife =c(30,150)
+
+# Coverage range (in %) (single range, can have multiple)
+Coverage = c(0.4, 1)
+
+# Maximum age (refer to reference survey age group)
+MaxAge = data.frame(MaxAge=c(10), 
+                    maxGroup=c(4))
+
+# Decay: define function, k and, efficacyB
+# ---- function: "constant" or "step" or "linear" or "exponential" or "weibull" or "hill" or "smooth-compact"
+# ---- k:         shape parameter of distribution. If not specified, default value of 1 is used.
+# ---- efficacyB: measure of variation
+Decay = data.frame(fundecay=c("hill"),
+                   kdecay=c(8),
+                   effB=c(1000))
+
+#####################
+### HEALTH SYSTEM ###
+
+# Coverage of healthcare system: probability of healthcare seeking of uncomplicated malaria cases (in %) 
+initial_access = data.frame(access=c(0.1,0.5)) 
+
+# Convert access to care to 5 day probabilities for use in XML files
+# Sources code from MMC project
+access = pmax(convert_access(initial_access * 100), 0.04)
+
+##################
+### ENTOMOLOGY ###
+
+# Seasonality type: monthly EIR vs. Fourier transformation (by month or with coefficients)
+# Check XML structure to match
 seasonality_type = "monthly"
 # seasonality_type = "Fourier"
 
-# !!! Depending on which seasonality is used, choose the correct scaffold.xml to upload
-# !!! If using Fourier, make sure number of coefficients correspond (example here I use a 6 coeff model with requires 5 a and 5 b values)
-
-# !!! Copy the scaffold.xml file for your experiment into the ./M3TPP/Experiments/"exp"/OM_jobs folder!!!
-# !!! Copy the same scaffold.xml file for your experiment into the .GROUP//M3TPP/Experiments/"exp"/OM_jobs folder!!!
-# !!! Copy a seasonality.csv file for your experiment into the ./M3TPP/Experiments/"exp"/ folder!!!
-
-######################################
-# Specify the desired parameter values 
-######################################
-
-chunk_size = 90000
-
-#############################
-# Categorical variables
-
-# Seasonality
+# Load seasonality file (path should be inside experiment)
 if(seasonality_type == "monthly"){
-  Seasonality =read.table(paste0("./Experiments/",exp,"/seasonality_monthly.txt"), sep="\t", header = TRUE)
+  # seasonality = read.table(paste0("./Experiments/",exp,"/seasonality_4_months_updatedMay302021.txt"), sep="\t", header = TRUE)
+  seasonality = read.table(paste0("./Experiments/",exp,"/exampleseason.txt"), sep="\t", header = TRUE)
 }
 if(seasonality_type == "Fourier"){
-  Seasonality =read.table(paste0("./Experiments/",exp,"/seasonality_Fourier_6_coeff.txt"), sep="\t", header = TRUE)
+  seasonality = read.table(paste0("./Experiments/",exp,"/seasonality_Fourier_6_coeff.txt"), sep="\t", header = TRUE)
 }
 
 # Biting patterns 
-Biting_pattern <- data.frame(Biting_pattern=c("Mali"),indoor=c(0.6),outdoor=c(0.4))
+biting_pattern <- data.frame(indoor=c(0.5),outdoor=c(0.5))
 
 # EIR
-EIR= data.frame(EIR=c(10))
+EIR= data.frame(EIR=c(10,100))
 
-# Max age intervention
-# age_groups (surveys age group)
-# 0.25 = group 1
-# 2 = group 2
-# 5 = group 3
-# 10 = group 4
-# 15 = group 5
-# 20 = group 6
-# 100 = group 7
-MaxAge = data.frame(MaxAge=c(10),maxGroup=c(4))
+###################
+### DIAGNOSTICS ###
 
-# Intervention decay
-LAIdecay <- data.frame(fundecay=c("weibull"),kdecay=c(1 ),LAIdecay=c("exp" ) )
+#############
+### MODEL ###
 
-# Coverage of healthcare system
-Access = data.frame(Access=c(0.1))  
 
-# Combine
-param_cat = list(Seasonality=Seasonality,
-                 Biting_pattern=Biting_pattern,
-                 EIR=EIR,
+##############################
+### GENERATE PARAMS TABLES ###
+##############################
+
+# Categorical variables
+param_cat = list(demography=demography,
+                 monitoring=monitoring,
                  MaxAge=MaxAge,
-                 LAIdecay=LAIdecay,
-                 Access=Access)
+                 Decay=Decay,
+                 access=access,
+                 seasonality=seasonality,
+                 biting_pattern=biting_pattern,
+                 EIR=EIR)
 
-#############################
-# Continuous variables and their ranges
+# Continuous variables
+param_ranges_cont = rbind(Coverage,Halflife,Efficacy)
 
-# Name of the experiment and parameters
-param_ranges_cont = rbind( Coverage = c(0.4, 1),
-                           Halflife =c(30,150),
-                           Efficacy= c(0.7,1) )
+# Number of continuous parameters to sample via lhs
+noSamples = 1
 
-###############################
-# Generate the parameter tables  
-###############################
-
-# no. of continuous parameters sampled via lhs
-noSamples = 5
-
-# no. of OM seeds per sample
-noSeeds=  2
+# Number of OM seeds per sample
+noSeeds=  1
 
 # Generate
-gen_paramtable(exp, param_ranges_cont,param_cat, noSamples, noSeeds,chunk_size)
+gen_paramtable(exp, param_ranges_cont, param_cat, noSamples, noSeeds, chunk_size)
 
-########################################################
-# Generate submission scripts and run the OM simulations
-########################################################\
+
+###########################################################
+### GENERATE SCENARIOS AND RUN OPEN MALARIA SIMULATIONS ###
+###########################################################
 
 # Number of outputs to get in OM folder
-2*nrow(Seasonality)*nrow(Biting_pattern)*nrow(EIR)*nrow(LAIdecay)*nrow(MaxAge)*nrow(Access)*noSamples*noSeeds
+2*nrow(demography)*nrow(monitoring)*nrow(MaxAge)*nrow(LAIdecay)*nrow(access)*nrow(seasonality)*nrow(biting_pattern)*nrow(EIR)*noSamples*noSeeds
 
 # Run
 genOMsimscripts(exp, chunk_size)
