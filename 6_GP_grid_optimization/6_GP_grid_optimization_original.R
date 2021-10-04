869ede4e29d3ab22d3cefeccbd2664bf6f7c3d57
Narimane Nekkab <nekkab0000@service06.cluster.bc2.ch>
2021-10-04 15:35:45 +0200

    LBM and JM updates

--- 6_GP_grid_optimization/6_GP_grid_optimization_original.R
+++ 6_GP_grid_optimization/6_GP_grid_optimization_original.R
@@ -24,6 +24,7 @@
 ###
 ### -------------------------------------------------------------------------
 
+
 ##############
 ### HEADER ###
 ##############
@@ -57,6 +58,7 @@ exp = "..."
 GROUP = "/scicore/home/penny/GROUP/M3TPP/"
 SIM_FOLDER = paste0(GROUP,exp,"/")
 
+
 ##################
 ### PARAMETERS ###
 ##################
@@ -66,22 +68,10 @@ ranges_file = paste0(GROUP,exp,"/param_ranges.RData")
 load(ranges_file)
 param_ranges_cont
 
-# Predicted variables
-predicted = "..."
-
 # Get postprocessing seed file name for GP file name
 seed_name = list.files(path = paste0(GROUP,exp,"/postprocessing/"), pattern = "seeds", full.names = FALSE)
 seed_name = sub('.txt', '', seed_name) 
 seed_name
-# GP file name
-gp_file = paste0("/scicore/home/penny/GROUP/M3TPP/",
-                 exp,
-                 "/gp/trained/",
-                 predicted,
-                 "/",
-                 seed_name,
-                 "_", predicted,
-                 "_cv.RData")
 
 # Variable to optimize
 optim_param = "..."
@@ -90,10 +80,13 @@ optim_param = "..."
 scale = T
 
 # Grid size
-n_grid = 100
+n_grid = 10
+# LBM: Can we have it so that you can specify a different resolution for the parameter being optimised vs. other parameters?
+# LBM: For example... n_grid = c("Coverage" = 20, "Halflife" = 20, "Efficacy" = 100)
 
-# Set target range size (& is per 1% jumps, 10 by 10% etc.)
+# Set target range size (1 is per 1% jumps, 10 by 10% etc.)
 target_range_size = 10
+
 
 ######################
 ### OUTPUT FOLDERS ###
@@ -103,7 +96,6 @@ target_range_size = 10
 opt_folder = paste0(SIM_FOLDER,"gp/GP_grid_optimization/")
 if(!dir.exists(opt_folder)){
   dir.create(opt_folder)
-  dir.create(paste0(opt_folder,predicted,"/"))
 }
 
 # # Create error folder
@@ -111,6 +103,8 @@ if(!dir.exists(opt_folder)){
 # if(!dir.exists(ERROR_FOLDER)){
 #   dir.create(ERROR_FOLDER)
 # }
+# LBM: Is there anything we might want to consider writing to an error file? E.g. printing which predicted outcome is being optimised?
+
 
 #######################################################
 ###      GRID SEARCH FUNCTION FOR OPTIMIZATION      ###
@@ -133,7 +127,6 @@ GP_grid_search_predictions <- function(predicted, gp_file, optim_param, scale, n
   }else{
     scale_params <- t(param_ranges_cont)
   }
-  scale_params
   
   ###################
   ### GRID SEARCH ###
@@ -142,22 +135,23 @@ GP_grid_search_predictions <- function(predicted, gp_file, optim_param, scale, n
   # Load GP model
   gp_result_name <- load(gp_file)
   gp_result <- cv_result$GP_model
-  summary(gp_result)
   
   # Create grid of parameter values to search
   ngrid <- c(NA, NA, NA)
   names(ngrid) <- rownames(param_ranges_cont)
   
   # Grid size
-  ngrid[!(names(ngrid) == optim_param)] <- n_grid # 10 to match optimization
-  ngrid[optim_param] <- param_ranges_cont[optim_param, 2] - param_ranges_cont[optim_param, 1] + 1 # number of integer values between min and max parameter ranges
+  ngrid[!(names(ngrid) == optim_param)] <- n_grid
+  ngrid[optim_param] <- n_grid * 10
+  # ngrid[optim_param] <- param_ranges_cont[optim_param, 2] - param_ranges_cont[optim_param, 1] + 1 # number of integer values between min and max parameter ranges
+  # LBM: Since we decided to have these specified manually, lines 154:159 should be something like this:
+  # LBM: ngrid <- rep(n_grid, length(rownames(param_ranges_cont)))
   
   # Create scenarios
   scenarios <- expand.grid(seq(scale_params[1, 1], scale_params[2, 1], length.out = ngrid[1]),
                            seq(scale_params[1, 2], scale_params[2, 2], length.out = ngrid[2]),
                            seq(scale_params[1, 3], scale_params[2, 3], length.out = ngrid[3]))
   names(scenarios) <- rownames(param_ranges_cont)
-  head(scenarios)
   
   ####################
   ### OPTIMIZATION ###
@@ -168,7 +162,6 @@ GP_grid_search_predictions <- function(predicted, gp_file, optim_param, scale, n
   scenarios$mean <- preds$mean
   scenarios$sd2 <- preds$sd2
   scenarios$nugs <- preds$nugs
-  head(scenarios)
   
   # Covert parameter values back to original scale
   if(scale == TRUE){
@@ -176,7 +169,6 @@ GP_grid_search_predictions <- function(predicted, gp_file, optim_param, scale, n
       scenarios[, i] <- scenarios[, i] * (param_ranges_cont[i, 2] - param_ranges_cont[i, 1]) + param_ranges_cont[i, 1]
     }
   }
-  head(scenarios)
   
   # Identify minimum parameter value required to reach target reduction
   scenarios$target_range <- floor(scenarios$mean/target_range_size)*target_range_size
@@ -192,22 +184,52 @@ GP_grid_search_predictions <- function(predicted, gp_file, optim_param, scale, n
   head(optimization_results)
   
   # Save tables
-  write.table(scenarios, file=paste0(opt_folder,predicted,"/",seed_name,"_",predicted,"_GP_grid_scenario_predictions.txt"))
-  write.table(optimization_results, file=paste0(opt_folder,predicted,"/",seed_name,"_",predicted,"_GP_grid_optimization_",optim_param,".txt"))
+  write.table(scenarios, file=paste0(opt_folder,predicted,"/",j,"_",predicted,"_GP_grid_scenario_predictions.txt")) # LBM: This file name is craaaazy long, what about something like '<exp name>_<scenario details>_<outcome>'
+  write.table(optimization_results, file=paste0(opt_folder,predicted,"/",j,"_",predicted,"_GP_grid_optimization_",optim_param,".txt")) 
+  # LBM: Both opt_folder and seed_name are arguments required here, better if we include them in the function inputs? I also run into the same problem here as above, in that for me seed_name is a string vector rather than string 
   
   return(list(scenarios, optimization_results))
 }
 
 
-##########################################################
-###      Performing optimization with grid search      ###
-##########################################################
+#############################################################################
+###      Performing optimization with grid search for all predictors      ###
+#############################################################################
 
-# Run for 1 predictor
-results = GP_grid_search_predictions(predicted, gp_file, optim_param, scale, n_grid, target_range_size)
-scenario_predictions = results[[1]]
-optimization_results = results[[2]]
-rm(results)
+# All predictors
+pred_list = c("prev_red_all","prev_red_210","prev_red_int","inc_red_05","inc_red_int","inc_red_all","inc_red_int_5mo")
+
+# Loop
+for(i in pred_list){
+  
+  print(i)
+  
+  # All seeds for each predictor
+  
+  for (j in seed_name){
+    
+    print(j)
+    
+    # Update GP file name
+    gp_file = paste0("/scicore/home/penny/GROUP/M3TPP/",exp,"/gp/trained/",i,"/",j,"_", i,"_cv.RData")
+    
+    # Create folder
+    if(!dir.create(paste0(opt_folder,i,"/"))){
+      dir.create(paste0(opt_folder,i,"/"))
+    }
+    
+    # Run
+    results = GP_grid_search_predictions(i, gp_file, optim_param, scale, n_grid, target_range_size)
+    scenario_predictions = results[[1]]
+    optimization_results = results[[2]]
+    rm(results)
+  }
+}
+
+
+#####################
+### VISUALIZATION ###
+#####################
 
 # Visualize grid search predictions for different targets
 p1 <- ggplot(scenario_predictions, 
@@ -226,38 +248,6 @@ p2 <- ggplot(optimization_results,
 p2
 
 
-#############################################################################
-###      Performing optimization with grid search for all predictors      ###
-#############################################################################
-
-# All predictors
-pred_list = c("prev_red_all","prev_red_210","prev_red_int","inc_red_05","inc_red_int","inc_red_all","inc_red_int_5mo")
-# Loop
-for(i in pred_list){
-  
-  print(i)
-  
-  # Update GP file name
-  gp_file = paste0("/scicore/home/penny/GROUP/M3TPP/",
-                   exp,
-                   "/gp/trained/",
-                   i,
-                   "/",
-                   seed_name,
-                   "_", i,
-                   "_cv.RData")
-  
-  # Create folder
-  if(!dir.create(paste0(opt_folder,i,"/"))){
-    dir.create(paste0(opt_folder,i,"/"))
-  }
-  
-  # Run
-  results = GP_grid_search_predictions(i, gp_file, optim_param, scale, n_grid, target_range_size)
-  scenario_predictions = results[[1]]
-  optimization_results = results[[2]]
-  rm(results)
-}
 
 
 
